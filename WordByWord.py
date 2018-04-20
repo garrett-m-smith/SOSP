d3344221189aa1b9bbd74398ba78b9cd0719f882
Garrett Smith <garrett.smith@uconn.edu>
2018-04-20 11:06:49 -0400

    Reduce dimensionality of state vector by having lexical identity dimensions plus head features instead of lexical item-specific features at each sentence position. Also add EMPTY lexical item so that sentences can be shorter than max_sentence_length.

--- WordByWord.py
+++ WordByWord.py
@@ -8,11 +8,21 @@ Word-by-word SOSP sentence processing tools
 
 The lexicon is basically a dictionary where the keys are attachment sites
 (head and dependent) and the values are NumPy arrays for the features.
+
+A treelet is a vector of head identities (lexical forms), head features, and
+dependent features.
+
+CHANGE (04/20): Instead of lexical-item-specific features at each position,
+I'll use one-hot word-identity dimensions at each position and a single bank
+of features for each position. This should greatly reduce the dimensionality
+of the system. ALSO: To allow for strings less than max_sent_length, need to
+add NULL lexical items at each position
 """
 
 # import numpy as np
 import yaml
 from itertools import product
+
 
 class Struct(object):
     def __init__(self, lex_file=None, features=None, max_sent_length=10):
@@ -36,24 +46,29 @@ class Struct(object):
     def _import_lexicon(self, file):
         with open(file, 'r') as stream:
             lex = yaml.safe_load(stream)
+        assert 'EMPTY' in lex.keys(), 'Lexicon must include EMPTY.'
         return lex
 
     def _get_dim_names(self):
         assert self.lexicon is not None, 'Must initialize lexicon.'
         per_position = []
         for word in self.lexicon:
-            for feat in self.features:
-                per_position.append('_'.join([word, feat]))
+            per_position.append(word)
+        for feat in self.features:
+            per_position.append(feat)
+        for word in self.lexicon:
             if self.lexicon[word]['dependents'] is not None:
                 for dep in self.lexicon[word]['dependents']:
                     for feat in self.features:
                         per_position.append('_'.join([word, dep, feat]))
         links = []
-        for pos_nr, word in product(range(self.max_sent_length), self.lexicon):
+        non_empty = {k: self.lexicon[k] for k in self.lexicon
+                     if k not in 'EMPTY'}
+        for pos_nr, word in product(range(self.max_sent_length), non_empty):
             other_positions = [x for x in range(self.max_sent_length)
                                if x != pos_nr]
             # Any word can appear at any position, so use whole lexicon here
-            for op, ow in product(other_positions, self.lexicon):
+            for op, ow in product(other_positions, non_empty):
                 if self.lexicon[ow]['dependents'] is not None:
                     for dep in self.lexicon[ow]['dependents']:
                         links.append('_'.join(['L', 'W' + str(pos_nr),
@@ -89,5 +104,6 @@ if __name__ == '__main__':
     sys.lexicon
 #    print(sys.dim_names)
     print('Number of dimensions', sys.ndim)
+    print(*sys.dim_names, sep='\n')
 
 # Creating 1-hot phonological form vectors: nwords = len(lex.keys())
